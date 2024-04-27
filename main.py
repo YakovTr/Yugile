cf53a85835d37ee9379bd909809d4d5bf7144481
YakovTr <treskovyr@gmail.com>
2024-04-27 17:44:59 +0300

    some new moments and readme

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def start(update: Update, context: CallbackContext):
         if not result:
             cursor.execute("INSERT INTO users (user_id, username) VALUES (?, ?)", (user.id, user.username))
             conn.commit()
-    await update.message.reply_text('Привет, я бот для контроля финансов')
+    await update.message.reply_text('Привет, я бот для контроля финансоn\n напишите @help')
 
 async def add_expense(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
@@ -54,10 +54,11 @@ async def view_balance(update: Update, context: CallbackContext):
 async def help(update: Update, context: CallbackContext):
     help_text = 'Список команд:\n\n' \
                 '/start - начать работу с ботом\n' \
-                '/add_expense - добавить расход\n' \
-                '/add_income - добавить доход\n' \
+                '/add_expense - добавить расход(чтобы добавить расход напишите в одном сообщении "@add_expense сумма категория)"\n' \
+                '/add_income - добавить доход(чтобы добавить доход напишите в одном сообщении "@add_expense сумма категория)"\n' \
                 '/view_balance - посмотреть баланс\n' \
                 '/view_expenses - посмотреть расходы по категориям\n' \
+                '/reset - очистить все доходы и расходы\n' \
                 '/view_incomes - посмотреть доходы по категориям\n'
     await update.message.reply_text(help_text)
 
@@ -89,11 +90,21 @@ async def view_incomes(update: Update, context: CallbackContext):
             income_text += f'{category}: {amount}\n'
         await update.message.reply_text(income_text)
 
+async def reset_data(update: Update, context: CallbackContext):
+    user_id = update.message.from_user.id
+    with sqlite3.connect('finance.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM expenses WHERE user_id = ?", (user_id,))
+        cursor.execute("DELETE FROM income WHERE user_id = ?", (user_id,))
+        conn.commit()
+    await update.message.reply_text('Все данные обновлены')
+
 
 def main():
     application = Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("reset", reset_data))
     application.add_handler(CommandHandler("help", help))
     application.add_handler(CommandHandler("add_expense", add_expense))
     application.add_handler(CommandHandler("add_income", add_income))
